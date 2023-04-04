36e097cfddde40d6253056a63a943e1b3f8046a1
Rivalzy <rivalds011@gmail.com>
2023-04-04 20:45:55 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,18 @@
 exports.getHome = (req, res, next) => {
     res.send('<h1>Selamat Datang di Beranda</h1>');
 };
+
+exports.middleware = (req,res,next) =>
+{
+    req.model = 'Anda berasil keluar'
+    next()
+}
+
+exports.getLogOut = (req, res, next) => {
+    console.log('ini request :>> ', req)
+    res.send({
+        response: req.model
+    })
+    next()
+};
+
--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Controller = require('../controllers/homeController');
+const sans = require('../controllers/homeController');
 
 const router = express.Router();
 
 router.get('/', Controller.getHome);
+router.get('/logout', Controller.getLogOut);
 
 module.exports = router;
